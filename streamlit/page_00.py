03286a79cf559d52f92d2259fbd1f6aa5646f16a
Tamires <159663594+Tamires-Anastacio@users.noreply.github.com>
2026-09-01 16:29:58 -0300

    Delete streamlit directory

--- streamlit/page_00.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-st.sidebar.markdown("Meu site 00 -- texto barra do menu")
-
-st.markdown("Minha página inicial 0 -- texto corpo da página")
